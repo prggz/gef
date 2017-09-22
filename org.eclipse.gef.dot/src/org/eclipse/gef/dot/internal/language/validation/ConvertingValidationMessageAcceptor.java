3ff157036ae9bba980a0af05f2e9c09107ffc349
Zoey Gerrit Prigge <zoey.prigge@uni-duesseldorf.de>
2017-09-22 09:09:13 +0200

    Adaptation handling nodes for calculating offset in ConvertingValidationMessageAcceptor

--- org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/validation/ConvertingValidationMessageAcceptor.java
+++ org.eclipse.gef.dot/src/org/eclipse/gef/dot/internal/language/validation/ConvertingValidationMessageAcceptor.java
@@ -1,8 +1,17 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2017 itemis AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Zoey Gerrit Prigge  - initial API and implementation (bug #454629)
+ *    
+ *******************************************************************************/
 package org.eclipse.gef.dot.internal.language.validation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -13,6 +22,10 @@ import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.validation.ValidationMessageAcceptor;
 
 /**
+ * This class provides a ValidationMessageAcceptor to be used with sub grammars.
+ * It can be used when validating the main grammar to translate validation
+ * errors.
+ * 
  * @author zgerritprigge
  *
  */
@@ -22,17 +35,23 @@ public class ConvertingValidationMessageAcceptor
 	private final EObject hostingEObject;
 	private final ValidationMessageAcceptor hostMessageAcceptor;
 	private final String userReadableIdentifier;
-	private int initialOffset;
+	private final int initialOffset;
 
 	/**
-	 * 
-	 * 
+	 * Constructs a Validation Message acceptor to 'translate' issues while
+	 * validating a sub grammar.
 	 * 
 	 * @param hostingEObject
+	 *            The Object in the main grammar that hosts the sub grammar
 	 * @param hostingFeature
+	 *            Can be null. If set, needs to be unique within hostingEObject
+	 *            (not a list, ...).
 	 * @param userReadableIdentifier
+	 *            A name for the sub grammar understood by the user
 	 * @param hostMessageAcceptor
+	 *            The validationMessageAcceptor of the host grammar
 	 * @param internalOffset
+	 *            Offset from begin of feature
 	 */
 	public ConvertingValidationMessageAcceptor(EObject hostingEObject,
 			EStructuralFeature hostingFeature, String userReadableIdentifier,
@@ -42,13 +61,17 @@ public class ConvertingValidationMessageAcceptor
 		this.hostMessageAcceptor = hostMessageAcceptor;
 		this.userReadableIdentifier = userReadableIdentifier;
 
-		this.initialOffset = getFirstNodeForEObject(hostingEObject,
-				hostingFeature).getOffset();
-		this.initialOffset = calculateOffset(internalOffset);
+		this.initialOffset = calculateInitialOffset(hostingFeature,
+				internalOffset);
 	}
 
 	/**
+	 * Helper Method to allow this message acceptor to handle syntax errors
+	 * issued while parsing the sub grammar as validation issues of the main
+	 * grammar.
+	 * 
 	 * @param error
+	 *            Error node from parsing.
 	 */
 	public void acceptSyntaxError(INode error) {
 		SyntaxErrorMessage errorMessage = error.getSyntaxErrorMessage();
@@ -63,10 +86,10 @@ public class ConvertingValidationMessageAcceptor
 	public void acceptError(String message, EObject object,
 			EStructuralFeature feature, int index, String code,
 			String... issueData) {
-		INode node = getFirstNodeForEObject(object, feature);
-		hostMessageAcceptor.acceptError(buildMessage("Semantic", message),
-				hostingEObject, calculateOffset(node.getOffset()),
-				node.getLength(), code, issueData);
+		for (INode node : getNodesForEObject(object, feature))
+			hostMessageAcceptor.acceptError(buildMessage("Semantic", message),
+					hostingEObject, calculateOffset(node.getOffset()),
+					node.getLength(), code, issueData);
 	}
 
 	@Override
@@ -81,10 +104,10 @@ public class ConvertingValidationMessageAcceptor
 	public void acceptInfo(String message, EObject object,
 			EStructuralFeature feature, int index, String code,
 			String... issueData) {
-		INode node = getFirstNodeForEObject(object, feature);
-		hostMessageAcceptor.acceptInfo(buildMessage("Semantic", message),
-				hostingEObject, calculateOffset(node.getOffset()),
-				node.getLength(), code, issueData);
+		for (INode node : getNodesForEObject(object, feature))
+			hostMessageAcceptor.acceptInfo(buildMessage("Semantic", message),
+					hostingEObject, calculateOffset(node.getOffset()),
+					node.getLength(), code, issueData);
 	}
 
 	@Override
@@ -99,10 +122,11 @@ public class ConvertingValidationMessageAcceptor
 	public void acceptWarning(String message, EObject object,
 			EStructuralFeature feature, int index, String code,
 			String... issueData) {
-		INode node = getFirstNodeForEObject(object, feature);
-		hostMessageAcceptor.acceptWarning(buildMessage("Semantic", message),
-				hostingEObject, calculateOffset(node.getOffset()),
-				node.getLength(), code, issueData);
+		for (INode node : getNodesForEObject(object, feature))
+			hostMessageAcceptor.acceptWarning(buildMessage("Semantic", message),
+					hostingEObject, calculateOffset(node.getOffset()),
+					node.getLength(), code, issueData);
+
 	}
 
 	@Override
@@ -113,22 +137,29 @@ public class ConvertingValidationMessageAcceptor
 				issueData);
 	}
 
+	private int calculateInitialOffset(EStructuralFeature hostingFeature,
+			int internalOffset) {
+		List<INode> nodes = getNodesForEObject(hostingEObject, hostingFeature);
+		if (nodes.size() != 1)
+			throw new RuntimeException(
+					"EObject has no node or feature not unique");
+		return nodes.get(0).getOffset() + internalOffset;
+	}
+
 	private int calculateOffset(int offset) {
 		return offset >= 0 && initialOffset >= 0 ? offset + initialOffset
 				: offset;
 	}
 
-	private INode getFirstNodeForEObject(EObject eObject,
+	private List<INode> getNodesForEObject(EObject eObject,
 			EStructuralFeature eStructuralFeature) {
 		List<INode> nodes = NodeModelUtils.findNodesForFeature(eObject,
 				eStructuralFeature);
-		if (nodes.size() != 1) {
-			System.err.println("Exactly 1 node is expected for the eObject "
-					+ eObject + ", but got " + nodes.size());
-			return null;
+		if (nodes.size() == 0) {
+			nodes = new ArrayList<INode>();
+			nodes.add(NodeModelUtils.findActualNodeFor(eObject));
 		}
-		INode node = nodes.get(0);
-		return node;
+		return nodes;
 	}
 
 	private String buildMessage(String errorType, String errorMessage) {
